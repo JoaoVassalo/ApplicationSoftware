8a92ebbbcf0aeea97fd8804d35e7d76037abf6e2
EndrwRafael <endriwrafaell@gmail.com>
2025-01-06 09:50:08 -0300

    Correct Catalog Variable
    Correct the catalog variables used in copernicus database

--- DbCatalogs/CopernicusCatalogs.py
+++ DbCatalogs/CopernicusCatalogs.py
@@ -13,6 +13,6 @@ copernicus_catalogs = {
     'reanalysis-era5-single-levels': Catalogs(
         typeproduct='reanalysis',
         initial=date(year=1940, month=1, day=1), final=date(year=2024, month=1, day=11),
-        variables=['10m u-component of wind', '10m v-component of wind']
+        variables=['10m_u_component_of_wind', '10m_v_component_of_wind']
     )
 }
