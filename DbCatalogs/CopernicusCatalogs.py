d107f1610cc52759d15cb4da6eb56443c27cc7ee
EndrwRafael <endriwrafaell@gmail.com>
2025-01-08 11:10:27 -0300

    Update Catalog Object
    Update Copernicus Catalog Object so now we have the region parameter indicating the data region available.

--- DbCatalogs/CopernicusCatalogs.py
+++ DbCatalogs/CopernicusCatalogs.py
@@ -2,17 +2,19 @@ from datetime import date, datetime
 
 
 class Catalogs:
-    def __init__(self, typeproduct: str, initial: datetime.date, final: datetime.date or str, variables: list[str]):
+    def __init__(self, typeproduct: str, initial: datetime.date, final: datetime.date or str, region: str, variables: list[str]):
         self.type = typeproduct
         self.i_date = initial
         self.f_date = final
+        self.region = region
         self.variables = variables
 
 
 copernicus_catalogs = {
     'reanalysis-era5-single-levels': Catalogs(
         typeproduct='reanalysis',
-        initial=date(year=1940, month=1, day=1), final=date(year=2024, month=1, day=11),
+        initial=date(year=1940, month=1, day=1), final=date(year=1940, month=1, day=1),
+        region='Global',
         variables=['10m_u_component_of_wind', '10m_v_component_of_wind']
     )
 }
