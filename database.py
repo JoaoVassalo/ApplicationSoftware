ed381f0ed99231aa4a1d9edd05d2156965418ca2
EndriwRafael <endriwrafaell@gmail.com>
2025-06-23 07:48:56 -0300

    Changed how database is used.
    Now the database is used only to save all the catalogs used to download netcdf data. The project path is now selected by the user within the application and can be changed during execution.

--- database.py
+++ database.py
@@ -8,11 +8,11 @@ from DbCatalogs.CopernicusCatalogs import copernicus_catalogs
 # Configuração do banco de dados
 Base = declarative_base()
 
-class Projeto(Base):
-    __tablename__ = 'projetos'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String, nullable=False)
-    caminho = Column(String, nullable=False)
+# class Projeto(Base):
+#     __tablename__ = 'projetos'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     nome = Column(String, nullable=False)
+#     caminho = Column(String, nullable=False)
 
 class Configuracao(Base):
     __tablename__ = 'configuracoes'
@@ -60,7 +60,8 @@ def criar_hycom_catalogo(nome, url, data_inicial, data_final, type_catalog, regi
     with Session() as session:
         catalogo_existente = session.query(HycomCatalogo).filter_by(nome=nome).first()
         if catalogo_existente:
-            raise ValueError(f"O catálogo '{nome}' já existe no banco de dados.")
+            # raise ValueError(f"O catálogo '{nome}' já existe no banco de dados.")
+            return
 
         novo_catalogo = HycomCatalogo(
             nome=nome,
@@ -80,7 +81,8 @@ def criar_copernicus_catalogo(nome, product, data_inicial, data_final, region_, 
     with Session() as session:
         catalogo_existente = session.query(CopernicusCatalogo).filter_by(nome=nome).first()
         if catalogo_existente:
-            raise ValueError(f"O catálogo '{nome}' já existe no banco de dados.")
+            # raise ValueError(f"O catálogo '{nome}' já existe no banco de dados.")
+            return
 
         novo_catalogo = CopernicusCatalogo(
             nome=nome,
@@ -101,37 +103,37 @@ def listar_catalogos():
         return catalogos
 
 # Funções utilitárias
-def criar_projeto(nome, caminho):
-    with Session() as session:
-        projeto_existente = session.query(Projeto).filter_by(nome=nome).first()
-        if projeto_existente:
-            raise ValueError(f"O projeto '{nome}' já existe no banco de dados.")
+# def criar_projeto(nome, caminho):
+#     with Session() as session:
+#         projeto_existente = session.query(Projeto).filter_by(nome=nome).first()
+#         if projeto_existente:
+#             raise ValueError(f"O projeto '{nome}' já existe no banco de dados.")
+#
+#         novo_projeto = Projeto(nome=nome, caminho=caminho)
+#         session.add(novo_projeto)
+#         session.commit()
+#         return novo_projeto
+#
+# def listar_projetos():
+#     with Session() as session:
+#         projetos = session.query(Projeto).all()
+#         return projetos
 
-        novo_projeto = Projeto(nome=nome, caminho=caminho)
-        session.add(novo_projeto)
-        session.commit()
-        return novo_projeto
-
-def listar_projetos():
-    with Session() as session:
-        projetos = session.query(Projeto).all()
-        return projetos
-
-def create_main_folder(default_folder=r"C:\Users\UDESC\Documents\Folder_NewProjeto"):
-    print('Criando Main Folder')
-    try:
-        with Session() as session:
-            folder_raiz = session.query(Configuracao).filter_by(chave="folder_raiz").first()
-            if not folder_raiz:
-                # Cria a pasta principal, se necessário
-                os.makedirs(default_folder, exist_ok=True)
-                folder_raiz = Configuracao(chave="folder_raiz", valor=default_folder)
-                session.add(folder_raiz)
-                session.commit()
-            return folder_raiz.valor
-    except Exception as e:
-        print(f"Erro ao criar o diretório: {e}")
-        return None
+# def create_main_folder(default_folder=r"C:\Users\UDESC\Documents\Folder_NewProjeto"):
+#     print('Criando Main Folder')
+#     try:
+#         with Session() as session:
+#             folder_raiz = session.query(Configuracao).filter_by(chave="folder_raiz").first()
+#             if not folder_raiz:
+#                 # Cria a pasta principal, se necessário
+#                 os.makedirs(default_folder, exist_ok=True)
+#                 folder_raiz = Configuracao(chave="folder_raiz", valor=default_folder)
+#                 session.add(folder_raiz)
+#                 session.commit()
+#             return folder_raiz.valor
+#     except Exception as e:
+#         print(f"Erro ao criar o diretório: {e}")
+#         return None
 
 # Inicialização do banco de dados
 DATABASE_URL = "sqlite:///projetos.db"  # Altere o caminho, se necessário
@@ -139,13 +141,13 @@ engine = create_engine(DATABASE_URL, echo=True)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
-if __name__ == '__main__':
-    create_main_folder()
+# if __name__ == '__main__':
+#     create_main_folder()
 
-    for key, item in hycom_catalogs.items():
-        criar_hycom_catalogo(nome=key, url=item.url, data_inicial=item.i_date, data_final=item.f_date,
-                             type_catalog=item.type, regiao=item.region, variaveis=item.variables)
+for key, item in hycom_catalogs.items():
+    criar_hycom_catalogo(nome=key, url=item.url, data_inicial=item.i_date, data_final=item.f_date,
+                         type_catalog=item.type, regiao=item.region, variaveis=item.variables)
 
-    for key, item in copernicus_catalogs.items():
-        criar_copernicus_catalogo(nome=key, product=item.type, data_inicial=item.i_date, data_final=item.f_date,
-                       region_=item.region, variaveis=item.variables)
+for key, item in copernicus_catalogs.items():
+    criar_copernicus_catalogo(nome=key, product=item.type, data_inicial=item.i_date, data_final=item.f_date,
+                   region_=item.region, variaveis=item.variables)
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
 import os
 from LoginPage import Ui_Form
-from database import Session, Projeto, Configuracao, HycomCatalogo, CopernicusCatalogo
+from database import Session, Configuracao, HycomCatalogo, CopernicusCatalogo  # , Projeto
 from index import Ui_MainWindow
 from PySide6.QtGui import QColor
 from PySide6 import QtWidgets
 
 
 class MainAppWindow(QMainWindow):
-    def __init__(self, project, Hcatalog, Ccatalog):
+    def __init__(self):  # , project, Hcatalog, Ccatalog
         super().__init__()
-        self.project = project
-        self.hycomCatalog = Hcatalog
-        self.copernicusCatalog = Ccatalog
+
+        # Inicializar sessão do banco de dados
+        self.session = Session()
+        catalogs_from_hycom = self.session.query(HycomCatalogo).all()
+        catalogs_from_copernicus = self.session.query(CopernicusCatalogo).all()
+
+        # self.project = project
+        self.hycomCatalog = catalogs_from_hycom
+        self.copernicusCatalog = catalogs_from_copernicus
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
@@ -25,123 +31,123 @@ class MainAppWindow(QMainWindow):
             getattr(self.ui, x).setGraphicsEffect(effect)
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
-
-        # Inicializar sessão do banco de dados
-        self.session = Session()
-
-        # Carregar listas de projetos
-        self.load_project_lists()
-
-        # Conectar botões às funcionalidades
-        self.ui.pushButton.clicked.connect(self.open_project)  # Abrir projeto
-        self.ui.pushButton_2.clicked.connect(self.create_project)  # Criar projeto
-        self.ui.pushButton_3.clicked.connect(self.delete_project)  # Deletar projeto
-
-    def load_project_lists(self):
-        """
-        Recarrega as listas de projetos nos dois QComboBox.
-        """
-        # Limpa os combo boxes
-        self.ui.comboBox.clear()
-        self.ui.comboBox_2.clear()
-
-        projetos = self.session.query(Projeto).all()
-        for projeto in projetos:
-            self.ui.comboBox.addItem(projeto.nome)  # Lista para abrir projeto
-            self.ui.comboBox_2.addItem(projeto.nome)  # Lista para deletar projeto
-
-    def create_project(self):
-        """
-        Cria um novo projeto, salva no banco e no disco.
-        """
-        nome_projeto = self.ui.lineEdit.text().strip()
-        if not nome_projeto:
-            QMessageBox.warning(self, "Erro", "O nome do projeto não pode estar vazio.")
-            return
-
-        folder_raiz = self.session.query(Configuracao).filter_by(chave="folder_raiz").first().valor
-        caminho_projeto = os.path.join(folder_raiz, nome_projeto)
-
-        try:
-            os.makedirs(caminho_projeto, exist_ok=False)
-        except FileExistsError:
-            QMessageBox.warning(self, "Erro", "Já existe um projeto com esse nome.")
-            return
-
-        # Salvar no banco de dados -------------------------------------------------------------------------------------
-        novo_projeto = Projeto(nome=nome_projeto, caminho=caminho_projeto)
-        self.session.add(novo_projeto)
-        self.session.commit()
-
-        QMessageBox.information(self, "Sucesso", f"Projeto '{nome_projeto}' criado em: {caminho_projeto}")
-
-        try:
-            projeto = self.session.query(Projeto).filter_by(nome=nome_projeto).first()
-            catalogs_from_hycom = self.session.query(HycomCatalogo).all()
-            catalogs_from_copernicus = self.session.query(CopernicusCatalogo).all()
-            self.mainapp = MainAppWindow(projeto, catalogs_from_hycom, catalogs_from_copernicus)
-            self.mainapp.show()
-            self.close()
-        except Exception as e:
-            raise QMessageBox.warning(self, "Erro", f"{e}")
-
-    def open_project(self):
-        """
-        Exibe informações do projeto selecionado.
-        """
-        projeto_selecionado = self.ui.comboBox.currentText()
-        if not projeto_selecionado:
-            QMessageBox.warning(self, "Erro", "Nenhum projeto selecionado.")
-            return
-
-        projeto = self.session.query(Projeto).filter_by(nome=projeto_selecionado).first()
-        if projeto:
-            catalogs_from_hycom = self.session.query(HycomCatalogo).all()
-            catalogs_from_copernicus = self.session.query(CopernicusCatalogo).all()
-            self.mainapp = MainAppWindow(projeto, catalogs_from_hycom, catalogs_from_copernicus)
-            self.mainapp.show()
-            self.close()
-        else:
-            QMessageBox.warning(self, "Erro", "Projeto não encontrado no banco de dados.")
-
-    def delete_project(self):
-        """
-        Remove um projeto selecionado do banco de dados e do disco.
-        """
-        projeto_selecionado = self.ui.comboBox_2.currentText()
-        if not projeto_selecionado:
-            QMessageBox.warning(self, "Erro", "Nenhum projeto selecionado.")
-            return
-
-        projeto = self.session.query(Projeto).filter_by(nome=projeto_selecionado).first()
-        if projeto:
-            confirmacao = QMessageBox.question(
-                self,
-                "Confirmar Exclusão",
-                f"Tem certeza de que deseja deletar o projeto '{projeto.nome}'?",
-                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
-            )
-            if confirmacao == QMessageBox.StandardButton.Yes:
-                # Remover do disco -------------------------------------------------------------------------------------
-                try:
-                    os.rmdir(projeto.caminho)
-                except OSError:
-                    QMessageBox.warning(self, "Erro", f"Não foi possível excluir a pasta '{projeto.caminho}'. Certifique-se de que está vazia.")
-                    return
-
-                # Remover do banco de dados ----------------------------------------------------------------------------
-                self.session.delete(projeto)
-                self.session.commit()
-
-                QMessageBox.information(self, "Sucesso", f"Projeto '{projeto.nome}' foi deletado.")
-                self.load_project_lists()
-        else:
-            QMessageBox.warning(self, "Erro", "Projeto não encontrado no banco de dados.")
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.ui = Ui_Form()
+#         self.ui.setupUi(self)
+#
+#         # Inicializar sessão do banco de dados
+#         self.session = Session()
+#
+#         # Carregar listas de projetos
+#         self.load_project_lists()
+#
+#         # Conectar botões às funcionalidades
+#         self.ui.pushButton.clicked.connect(self.open_project)  # Abrir projeto
+#         self.ui.pushButton_2.clicked.connect(self.create_project)  # Criar projeto
+#         self.ui.pushButton_3.clicked.connect(self.delete_project)  # Deletar projeto
+#
+#     def load_project_lists(self):
+#         """
+#         Recarrega as listas de projetos nos dois QComboBox.
+#         """
+#         # Limpa os combo boxes
+#         self.ui.comboBox.clear()
+#         self.ui.comboBox_2.clear()
+#
+#         projetos = self.session.query(Projeto).all()
+#         for projeto in projetos:
+#             self.ui.comboBox.addItem(projeto.nome)  # Lista para abrir projeto
+#             self.ui.comboBox_2.addItem(projeto.nome)  # Lista para deletar projeto
+#
+#     def create_project(self):
+#         """
+#         Cria um novo projeto, salva no banco e no disco.
+#         """
+#         nome_projeto = self.ui.lineEdit.text().strip()
+#         if not nome_projeto:
+#             QMessageBox.warning(self, "Erro", "O nome do projeto não pode estar vazio.")
+#             return
+#
+#         folder_raiz = self.session.query(Configuracao).filter_by(chave="folder_raiz").first().valor
+#         caminho_projeto = os.path.join(folder_raiz, nome_projeto)
+#
+#         try:
+#             os.makedirs(caminho_projeto, exist_ok=False)
+#         except FileExistsError:
+#             QMessageBox.warning(self, "Erro", "Já existe um projeto com esse nome.")
+#             return
+#
+#         # Salvar no banco de dados -------------------------------------------------------------------------------------
+#         novo_projeto = Projeto(nome=nome_projeto, caminho=caminho_projeto)
+#         self.session.add(novo_projeto)
+#         self.session.commit()
+#
+#         QMessageBox.information(self, "Sucesso", f"Projeto '{nome_projeto}' criado em: {caminho_projeto}")
+#
+#         try:
+#             projeto = self.session.query(Projeto).filter_by(nome=nome_projeto).first()
+#             catalogs_from_hycom = self.session.query(HycomCatalogo).all()
+#             catalogs_from_copernicus = self.session.query(CopernicusCatalogo).all()
+#             self.mainapp = MainAppWindow(projeto, catalogs_from_hycom, catalogs_from_copernicus)
+#             self.mainapp.show()
+#             self.close()
+#         except Exception as e:
+#             raise QMessageBox.warning(self, "Erro", f"{e}")
+#
+#     def open_project(self):
+#         """
+#         Exibe informações do projeto selecionado.
+#         """
+#         projeto_selecionado = self.ui.comboBox.currentText()
+#         if not projeto_selecionado:
+#             QMessageBox.warning(self, "Erro", "Nenhum projeto selecionado.")
+#             return
+#
+#         projeto = self.session.query(Projeto).filter_by(nome=projeto_selecionado).first()
+#         if projeto:
+#             catalogs_from_hycom = self.session.query(HycomCatalogo).all()
+#             catalogs_from_copernicus = self.session.query(CopernicusCatalogo).all()
+#             self.mainapp = MainAppWindow(projeto, catalogs_from_hycom, catalogs_from_copernicus)
+#             self.mainapp.show()
+#             self.close()
+#         else:
+#             QMessageBox.warning(self, "Erro", "Projeto não encontrado no banco de dados.")
+#
+#     def delete_project(self):
+#         """
+#         Remove um projeto selecionado do banco de dados e do disco.
+#         """
+#         projeto_selecionado = self.ui.comboBox_2.currentText()
+#         if not projeto_selecionado:
+#             QMessageBox.warning(self, "Erro", "Nenhum projeto selecionado.")
+#             return
+#
+#         projeto = self.session.query(Projeto).filter_by(nome=projeto_selecionado).first()
+#         if projeto:
+#             confirmacao = QMessageBox.question(
+#                 self,
+#                 "Confirmar Exclusão",
+#                 f"Tem certeza de que deseja deletar o projeto '{projeto.nome}'?",
+#                 QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+#             )
+#             if confirmacao == QMessageBox.StandardButton.Yes:
+#                 # Remover do disco -------------------------------------------------------------------------------------
+#                 try:
+#                     os.rmdir(projeto.caminho)
+#                 except OSError:
+#                     QMessageBox.warning(self, "Erro", f"Não foi possível excluir a pasta '{projeto.caminho}'. Certifique-se de que está vazia.")
+#                     return
+#
+#                 # Remover do banco de dados ----------------------------------------------------------------------------
+#                 self.session.delete(projeto)
+#                 self.session.commit()
+#
+#                 QMessageBox.information(self, "Sucesso", f"Projeto '{projeto.nome}' foi deletado.")
+#                 self.load_project_lists()
+#         else:
+#             QMessageBox.warning(self, "Erro", "Projeto não encontrado no banco de dados.")
 
 
 shadow_elements = {
@@ -151,7 +157,7 @@ shadow_elements = {
     'download_page',
     'view_page',
     'simulation_page',
-    'settings_page',
+    'frame_25',  # setting page
     'file_page',
     'frame_to_info',
     'coord_date_file_widgets',
@@ -207,6 +213,18 @@ if __name__ == "__main__":
             #menu_button {
                 background-color: #2C423F;
                 border: none;
+            }
+            
+            [windowbuttons="true"] {
+                background-color: #2C423F;
+                border-radius: 14px;
+                border: 2px solid #E0E2DB;
+                width: 25px;
+                height: 25px;
+            }
+            
+            [windowbuttons="true"]:hover {
+                background-color: #E0E2DB;
             }
             
             [DownloadCommomFrame="true"] {
@@ -551,6 +569,6 @@ if __name__ == "__main__":
             ------------------------------------------------------------------------------------------------------------
             */
         """)
-    window = MainWindow()
+    window = MainAppWindow()
     window.show()
     app.exec()
